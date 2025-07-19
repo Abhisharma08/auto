e0162f22118c4309ec6f1d18010d48c58c6e9b17
Abhisharma08 <aksharma223@gmail.com>
2025-07-19 17:42:33 +0530

    DONE

--- auto.py
+++ auto.py
@@ -10,7 +10,7 @@ api_hash = os.getenv("API_HASH")
 
 app = Client("userbot_session", api_id=api_id, api_hash=api_hash)
 
-AUTO_REPLY = "👋 Hi! I'm away right now or may be no more available here. Will reply later if..."
+AUTO_REPLY = "...........Hi.....Byee........"
 @app.on_message(filters.private & ~filters.me)
 async def auto_reply(client, message):
     await message.reply_text(AUTO_REPLY)
